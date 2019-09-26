21a918d2f31a33dba24a94a7e1f4c8cd04dbf2c9
FRANCISCA HOHLBERG <fhohlberg@uc.cl>
2019-09-26 18:40:43 -0400

    inicio

---
+++ README.md
@@ -0,0 +1,6 @@
+**Proyecto Grupo 52**
+Integrantes: Francisca Hohlberg, Elisa Prieto, Araceli Rojas
+# Grupo52-proyecto
+
+La dirección de la aplicación montada en Heroku (Hello World!) es https://grupo52-proyecto.herokuapp.com/home/index
+
---
+++ db/migrate/20190926005056_create_plates.rb
@@ -0,0 +1,13 @@
+class CreatePlates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plates do |t|
+      t.string :name
+      t.integer :price
+      t.string :description
+      t.integer :portions
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
